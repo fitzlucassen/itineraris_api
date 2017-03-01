33d90e0f883929b5995f336292578615be7b7473
Thibault Dulon <t.dulon@betclicgroup.com>
2017-03-01 11:48:47 +0100

    upload ok

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'uploads')));
 
 app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
--- routes/steps.js
+++ routes/steps.js
@@ -1,22 +1,37 @@
 var express = require('express');
 var crypto = require('crypto');
+var multer = require('multer');
 var db = require('../helpers/db')();
 var dateHelper = require('../helpers/date')();
 
 var router = express.Router();
 
+var storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, './uploads/')
+	},
+	filename: function (req, file, cb) {
+		cb(null, Date.now() + '-' + file.originalname)
+	}
+});
+var upload = multer({
+	dest: './uploads/',
+	storage: storage
+});
+var uploadType = upload.array('uploads[]', 12);
+
 /***********************/
 /* GET itinerary steps */
 /***********************/
 router.get('/itinerary/:itineraryid', function (req, res, next) {
-    // Get params from client
-    var itineraryId = req.params.itineraryid;
+	// Get params from client
+	var itineraryId = req.params.itineraryid;
 
-    // Get itinerary steps of an itinerary in database with these parameters if exists
+	// Get itinerary steps of an itinerary in database with these parameters if exists
 	var steps = db.byFields('step', {
 		id_Itinerary: itineraryId
-	}, function(error, results, fields){
-		if(error != null)
+	}, function (error, results, fields) {
+		if (error != null)
 			res.respond(error, 500);
 		else
 			res.respond(results);
@@ -24,14 +39,14 @@ router.get('/itinerary/:itineraryid', function (req, res, next) {
 });
 
 router.get('/:stepid', function (req, res, next) {
-    // Get params from client
-    var stepId = req.params.stepid;
+	// Get params from client
+	var stepId = req.params.stepid;
 
-    // Get itinerary step in database with these parameters if exists
+	// Get itinerary step in database with these parameters if exists
 	var itineraries = db.byFields('step', {
 		id: stepId
-	}, function(error, results, fields){
-		if(error != null)
+	}, function (error, results, fields) {
+		if (error != null)
 			res.respond(error, 500);
 		else
 			res.respond(results.length > 0 ? results[0] : null);
@@ -41,7 +56,7 @@ router.get('/:stepid', function (req, res, next) {
 /*****************************/
 /* PUT update itinerary step */
 /*****************************/
-router.put('/:stepid', function(req, res, next){
+router.put('/:stepid', function (req, res, next) {
 	// Get params from client
 	var stepId = req.params.stepid;
 
@@ -51,17 +66,17 @@ router.put('/:stepid', function(req, res, next){
 
 	// Update the itinerary step in database
 	db.update('step', {
-		city: city, 
-		date: date, 
-		description: description, 
+		city: city,
+		date: date,
+		description: description,
 	}, {
-		id: stepId
-	}, function(error, results, fields){
-		if(error != null)
-			res.respond(error, 500);
-		else
-			res.respond([]);
-	});
+			id: stepId
+		}, function (error, results, fields) {
+			if (error != null)
+				res.respond(error, 500);
+			else
+				res.respond([]);
+		});
 });
 
 /***************************/
@@ -76,15 +91,15 @@ router.post('/', function (req, res, next) {
 
 	// Insert the itinerary step in database
 	db.add('step', {
-		city: city, 
-		description: description, 
-        id_Itinerary: itineraryId,
+		city: city,
+		description: description,
+		id_Itinerary: itineraryId,
 		date: date
-	}, function(error, results, fields){
-		if(error != null)
+	}, function (error, results, fields) {
+		if (error != null)
 			res.respond(error, 500);
 		else
-			res.respond({id: results.insertId});
+			res.respond({ id: results.insertId });
 	});
 });
 
@@ -97,15 +112,35 @@ router.delete('/:stepid', function (req, res, next) {
 
 	// Delete the itinerary step in database
 	db.remove('step', {
-        id: stepId,
-	}, function(error, results, fields){
-		if(error != null)
+		id: stepId,
+	}, function (error, results, fields) {
+		if (error != null)
 			res.respond(error, 500);
 		else {
-            res.respond([]);
+			res.respond([]);
 		}
 	});
 });
 
+/************************/
+/* POST add step images */
+/************************/
+router.post('/:stepid/images', uploadType, function (req, res) {
+	// Get params from client
+	var stepId = req.params.stepid;
+
+	req.files.forEach(function (element) {
+		db.add('picture', {
+			id_Step: stepId,
+			url: 'uploads/' + element.filename,
+			date: dateHelper.getDateTime()
+		}, function (error, results, fields) {
+			if (error != null)
+				res.respond(error, 500);
+			else
+				res.send(req.files);
+		});
+	});
+});
+
 module.exports = router;
- 
